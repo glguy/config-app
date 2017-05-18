f1782ac0ebb973d90935c52be94d5e3183bce3b4
David Johnson <djohnson.m@gmail.com>
2017-05-17 21:12:54 -0500

    Make things work

--- Main.hs
+++ Main.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Example
 
+import Config
+import Data.JSString
+import Data.JSString.Text
+import GHCJS.Foreign.Callback
+import GHCJS.Types
+
+foreign import javascript unsafe "$1.addEventListener('click', $2)"
+  addClickListener :: JSVal -> Callback (IO ()) -> IO ()
+
+foreign import javascript unsafe "$r = document.getElementById($1);"
+  getElement :: JSString -> IO JSVal
+
+foreign import javascript unsafe "$r = $1.value;"
+  getValue :: JSVal -> IO JSString
+
+foreign import javascript unsafe "console.log($1);"
+  clog :: JSString -> IO ()
+
 main :: IO ()
-main = print example
+main = do
+  area <- getElement "area"
+  getElement "btn" >>= \btn ->
+    (addClickListener btn =<< asyncCallback (go area))
+    where
+      go area = do
+        areaValue <- textFromJSString <$> getValue area
+        clog $ pack $ show (parse areaValue)
+
+        
+    
